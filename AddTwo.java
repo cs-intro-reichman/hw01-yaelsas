16cd09039dcd5e5f15cc498422cc2b942276556c
Yael Sasson <yael.sasson01@post.runi.ac.il>
2023-12-22 21:40:03 +0200

    added remark

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,7 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
+		// Put your code here
 		int x = Integer.parseInt(args[0]);
 	    int y = Integer.parseInt(args[1]);
 		// prints the result of 2 given integers.
